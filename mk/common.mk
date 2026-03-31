f17a52f66320196d53dda4519b328fe98e5acc9a
Benoit LAFFITTE <b.laffitte@maif.fr>
2026-03-31 22:35:31 +0200

    refactor: remove Docker build and scan targets from Makefile

--- mk/common.mk
+++ mk/common.mk
@@ -6,7 +6,3 @@ CARGO ?= cargo
 DOCKER ?= docker
 TRIVY ?= trivy
 
-SERVER_DOCKERFILE ?= Dockerfile.server
-WORKER_DOCKERFILE ?= Dockerfile.worker
-SERVER_IMAGE ?= tardigrade-server:local
-WORKER_IMAGE ?= tardigrade-worker:local
